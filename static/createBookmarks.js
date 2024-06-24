aa5107a248e26f97909deb9d2f0bf75b866b4292
mistraali <bartlomiejkapusta@gmail.com>
2024-06-24 16:08:53 +0200

    bookmarks fix

--- static/createBookmarks.js
+++ static/createBookmarks.js
@@ -1,8 +1,12 @@
-document.querySelector('#bookmarks').innerHTML = `
-    <a id="p0" href="/" class="list-group-item list-group-item-action active">Strona główna</a>
-    `
-    for(i=1; i<=12; i++){
-        document.querySelector('#bookmarks').innerHTML += `
-        <a id="p${i}" href="/k${i}" class="list-group-item list-group-item-action active">Księga ${i}</a>
+function createBookmarks(bookNumber) {
+    document.querySelector('#bookmarks').innerHTML = `
+        <a id="p0" href="/" class="list-group-item list-group-item-action">Strona główna</a>
         `
+        for(i=1; i<=12; i++){
+            document.querySelector('#bookmarks').innerHTML += `
+            <a id="p${i}" href="/k${i}" class="list-group-item list-group-item-action">Księga ${i}</a>
+            `
+    }
+    console.log()
+    document.getElementById(`p${bookNumber}`).classList.add("active");
 }
